d57416a3c988a67b77ac4333b312c597bf92a4ab
suxxus <suxxus@outlook.com>
2020-03-05 17:58:34 +0100

    Add studie

---
+++ studies/from-life-of-file-two/precache-manifest.84415b0d0a77288dd66166061ab3c80f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "2fbf3988cff12ff5510ea51631ba69b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "af54ab263ec013ba4873",
+    "url": "/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "af54ab263ec013ba4873",
+    "url": "/static/js/main.af54ab26.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
