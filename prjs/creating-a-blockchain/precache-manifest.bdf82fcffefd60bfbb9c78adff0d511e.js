c571f647375104dddd4d98273679caf39e3dc96d
suxxus <suxxus@outlook.com>
2021-01-02 19:22:54 +0100

    Add creating-a-blockchain studie

---
+++ prjs/creating-a-blockchain/precache-manifest.bdf82fcffefd60bfbb9c78adff0d511e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "ff6ffb470313ed818e78c7f23e446b98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "a8c9c3667ef8a14fba83",
+    "url": "/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "a8c9c3667ef8a14fba83",
+    "url": "/static/js/main.a8c9c366.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
